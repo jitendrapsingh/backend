72a0414a559e3a86f6cc2f271c44db3226883462
Jitendra Singh <dba.jitendra@gmail.com>
2020-09-07 10:55:14 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -10,10 +10,10 @@ variable "PATH_TO_PUBLIC_KEY" {
 }
 
 variable "AWS_AMI" {
-  #type = "map"
+  type = "map"
   default = {
-   #eu-west-1 = "ami-028f419d2bf90fe5d"
-   #eu-west-1 = "ami-0a7c31280fbd23a86"
-   #eu-west-1 = "ami-051cbea0e7660063d"
+   "eu-west-1" = "ami-028f419d2bf90fe5d"
+   "eu-west-1" = "ami-0a7c31280fbd23a86"
+   "eu-west-1" = "ami-051cbea0e7660063d"
   }
 }
